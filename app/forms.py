53da42a773a592ce42c7e0beb59353b436d27ab7
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-17 15:03:21 +0300

    add new code in formspy

---
+++ app/forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+from flask_login import current_user
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=35)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Такое имя уже существует')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Такая почта уже используется')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Запомни меня')
+    submit = SubmitField('Login')
+
+# Добавляем форму для редактирования профиля
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=35)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('New Password', validators=[Length(min=6, max=60, message="Enter a new password")])
+    confirm_password = PasswordField('Confirm New Password', validators=[EqualTo('password')])
+    submit = SubmitField('Update')
+
+    # Проверка уникальности нового имени пользователя
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Такое имя уже существует')
+
+    # Проверка уникальности нового email
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email_check = User.query.filter_by(email=email.data).first()
+            if email_check:
+                raise ValidationError('Такая почта уже используется'
